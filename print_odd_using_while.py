201b262c3441ee9b3a3cadadcc4e3dce6cc032b3
Linden <lindenpowell218@gmail.com>
2025-06-14 09:36:01 +0800

    Odd using while

---
+++ print_odd_using_while.py
@@ -0,0 +1,8 @@
+class PrintOddWhile:
+    def __init__(self):
+        self.num = 1
+    def PowRun(self):
+        while True:
+            if self.num <= 100:
+                print(self.num)
+                self.num += 2
---
+++ print_odd_using_while_result.py
@@ -0,0 +1,3 @@
+from print_odd_using_while import PrintOddWhile
+run = PrintOddWhile()
+run.PowRun()
